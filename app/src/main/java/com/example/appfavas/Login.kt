e80a17b1d572c4b44ea890be47e22c75968da25f
Yizark30 <yizark.farrach13455@est.uca.edu.ni>
2023-04-28 14:59:50 -0600

    Creacion de UI Login Y Registrarse

---
+++ app/src/main/java/com/example/appfavas/Login.kt
@@ -0,0 +1,42 @@
+package com.example.appfavas
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import com.example.appfavas.databinding.FragmentLoginBinding
+
+
+class Login : Fragment() {
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            val navController = view?.findNavController()
+            navController?.navigate(R.id.PantallaLogin)
+
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLoginBinding.inflate(layoutInflater)
+        iniciar()
+        return binding.root
+    }
+
+    private fun iniciar() {
+        binding.btnRegLogin.setOnClickListener {
+            Navigation.findNavController(binding.root).navigate(R.id.PantallaRegistrarse)
+        }
+
+    }
+
+}
